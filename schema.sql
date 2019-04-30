ad586fbece108b055ebb17c64d88bc46bee7d09f
BenShep <19ute14@gmail.com>
2019-04-30 15:57:26 -0600

    working js

--- schema.sql
+++ schema.sql
@@ -1,45 +1,45 @@
 
-CREATE DATABASE bamazon_DB;
+CREATE DATABASE Bamazon_DB;
+USE Bamazon_DB;
 
-USE bamazon_DB
-
-CREATE TABLE products(
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
-  price INT default 0,
-  stock_quantity INT default 0,
-  PRIMARY KEY (id)
+-- Create a table called 'products' which will contain the store inventory --
+CREATE TABLE products (
+	product_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (product_id)
 );
 
-INSERT INTO products (product_name,department_name, price, stock_quantity)
-VALUES ("Dog Bones", "Pets", 9.99, 20);
+INSERT INTO products (product_id, product_name ,department_name, price, stock_quantity)
+VALUES (1,"Dog Bones", "Pets", 9.99, 20);
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Bang Energy Drink", "Food/Beverage" 2.99, 120);
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (2,"Bang Energy Drink", "Food/Beverage" 2.99, 120);
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Apple Fritter", "Food/Beverage", 3.50, 25); 
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (3,"Apple Fritter", "Food/Beverage", 3.50, 25); 
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Nike Football", "Outdoor", 19.99, 12); 
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (4,"Nike Football", "Outdoor", 19.99, 12); 
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Surround Sound System", "Entertainment", 499.99, 3); 
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (5,"Surround Sound System", "Entertainment", 499.99, 3); 
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Dell XPS-15", "Technology", 1599.99, 5);
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (6,"Dell XPS-15", "Technology", 1599.99, 5);
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Smart Water", "Food/Beverage", 2.50, 133); 
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (7,"Smart Water", "Food/Beverage", 2.50, 133); 
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Dog Bed", "Pets", 45.99, 16);
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (8,"Dog Bed", "Pets", 45.99, 16);
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Samsung 65 series TV", "Entertainment", 1200.00, 10);
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (9,"Samsung 65 series TV", "Entertainment", 1200.00, 10);
 
-INSERT INTO products  (product_name,department_name, price, stock_quantity)
-VALUES ("Beach Chairs", "Outdoors", 49.99, 24);
+INSERT INTO products  (product_id, product_name, department_name, price, stock_quantity)
+VALUES (10,"Beach Chairs", "Outdoors", 49.99, 24);
  
 
